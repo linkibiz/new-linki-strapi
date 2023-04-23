ecc79102af63e17928848c973597e13136af3518
cristhian vargas <cristhian.a.vargas@gmail.com>
2023-04-23 10:21:49 -0500

    ajustes de plugins

---
+++ config/plugins.js
@@ -0,0 +1,26 @@
+module.exports = ({ env }) => ({
+  //...
+  'import-export-entries': {
+    enabled: true,
+  },
+  'strapi-plugin-populate-deep': {
+    config: {
+      defaultDepth: 3, // Default is 5
+    }
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
